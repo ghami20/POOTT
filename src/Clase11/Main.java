bb5f8f602754bc0b6917722c8c380108affc37b3
Gamaliel Quiroz <83394687+ghami20@users.noreply.github.com>
2023-09-25 14:20:08 -0300

    ProgramaFechaParcial

--- src/Clase11/Main.java
+++ src/Clase11/Main.java
@@ -1,12 +1,19 @@
 package Clase11;
 
+import java.time.LocalDate;
+
 import javax.swing.JOptionPane;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JOptionPane.showMessageDialog(null, "Hola mundo");
+		LocalDate fecha =  LocalDate.now();
+		
+		System.out.println(fecha.getDayOfMonth());
+		if (fecha.getDayOfMonth()== 9 ) {
+			JOptionPane.showMessageDialog(null, "Hay parcial");
+		}
 	}
 
 }
